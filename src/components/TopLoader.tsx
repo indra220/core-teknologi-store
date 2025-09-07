d7cfb43c28dc0d03b683901fc3c41abb3f4f51e0
indra220 <m.indra.p42@gmail.com>
2025-09-07 20:33:34 +0700

    Fix error toploader

--- src/components/TopLoader.tsx
+++ src/components/TopLoader.tsx
@@ -1,41 +1,41 @@
+// src/components/TopLoader.tsx
+
 'use client';
 
 import { useEffect } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 import NProgress from 'nprogress';
+import Link from 'next/link';
+
+// Komponen Link kustom yang akan memicu NProgress
+export const CustomLink = (props: React.ComponentProps<typeof Link>) => {
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    // Cek jika ini bukan navigasi ke halaman yang sama
+    if (props.href !== window.location.pathname) {
+      NProgress.start();
+    }
+    if (props.onClick) {
+      props.onClick(event);
+    }
+  };
+
+  return <Link {...props} onClick={handleClick} />;
+};
+
 
 export default function TopLoader() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
+  // Efek ini akan menghentikan loader setelah halaman selesai dimuat
   useEffect(() => {
     NProgress.done();
   }, [pathname, searchParams]);
 
+  // Efek ini hanya berjalan sekali untuk konfigurasi
   useEffect(() => {
     NProgress.configure({ showSpinner: false });
-
-    const handleAnchorClick = (event: MouseEvent) => {
-      const targetUrl = (event.currentTarget as HTMLAnchorElement).href;
-      const currentUrl = window.location.href;
-      if (targetUrl !== currentUrl) {
-        NProgress.start();
-      }
-    };
-    
-    const handleMutation = () => {
-        const anchorElements = document.querySelectorAll('a');
-        anchorElements.forEach((anchor) => anchor.addEventListener('click', handleAnchorClick));
-    };
-
-    const mutationObserver = new MutationObserver(handleMutation);
-    mutationObserver.observe(document, { childList: true, subtree: true });
-
-    return () => {
-      mutationObserver.disconnect();
-      document.querySelectorAll('a').forEach((anchor) => anchor.removeEventListener('click', handleAnchorClick));
-    };
   }, []);
 
-  return null;
+  return null; // Komponen ini tidak me-render UI apa pun
 }
